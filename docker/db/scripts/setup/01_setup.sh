81a610d5b20d3ba776eb80ecd99a877f42defe77
大熊　諒 <r.okuma@a2251-222.opt-sys.local>
2022-10-24 18:36:19 +0900

    Oracle DB に変更

---
+++ docker/db/scripts/setup/01_setup.sh
@@ -0,0 +1,2 @@
+# pdb作成用ディレクトリ
+sudo -u oracle mkdir /opt/oracle/oradata/XE/pdb01
---
+++ docker/db/scripts/setup/04_create_table.sql
@@ -0,0 +1,20 @@
+-- PDBへ切り替え
+ALTER SESSION SET CONTAINER=pdb01;
+
+-- テーブル作成するユーザ(変数定義)
+DEFINE USER_NAME='SpringBoot';
+
+-- テーブル作成
+CREATE TABLE &USER_NAME..todo
+(
+    id NUMBER GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR2(300),
+    CONSTRAINT pk1 PRIMARY KEY(id)
+ ) 
+;
+
+-- データ挿入
+INSERT INTO &USER_NAME..todo(name) values('初期データ１');
+INSERT INTO &USER_NAME..todo(name) values('初期データ２');
+INSERT INTO &USER_NAME..todo(name) values('初期データ３');
+COMMIT;
