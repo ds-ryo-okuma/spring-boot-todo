5daac925a43d96e302513718e46791655fd1f5f7
大熊　諒 <r.okuma@a2251-222.opt-sys.local>
2022-10-25 11:35:48 +0900

    コントローラからサービスを呼び出すように変更

--- src/main/java/com/example/springboottodo/service/TodoService.java
+++ src/main/java/com/example/springboottodo/service/TodoService.java
@@ -1,5 +1,7 @@
 package com.example.springboottodo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,16 @@ public class TodoService {
 	@Autowired
 	TodoRepository todoRepository;
 	
+	public void save(Todo todo) {
+		todoRepository.save(todo);
+	}
+	
 	public Iterable<Todo> serchAll() {
 		return todoRepository.findAll();
+	}
+	
+	public Optional<Todo> findById(Integer id) {
+		return todoRepository.findById(id);
 	}
 	
 	public void deleteById(Integer id) {
